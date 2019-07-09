5eb089863b2a766ca804665a5918f267c85695b9
wangrui <mistra3030000>
2019-07-09 23:53:04 +0800

    ws server

--- src/main/java/com/mistra/nw/helloNettyServer/CustomHandler.java
+++ src/main/java/com/mistra/nw/helloNettyServer/CustomHandler.java
@@ -22,19 +22,81 @@ public class CustomHandler extends SimpleChannelInboundHandler<HttpObject> {
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
         //获取channel
         Channel channel = channelHandlerContext.channel();
-        //打印客户端请求地址
-        System.out.println(channel.remoteAddress());
-        //设置响应内容
-        ByteBuf content = Unpooled.copiedBuffer("hello mistra", CharsetUtil.UTF_8);
+        if (httpObject instanceof  HttpRequest){
+            //打印客户端请求地址
+            System.out.println(channel.remoteAddress());
+            //设置响应内容
+            ByteBuf content = Unpooled.copiedBuffer("hello mistra", CharsetUtil.UTF_8);
 
-        //构建一个HttpResponse
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
-        //设置响应内容的格式  文字 图片
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-        //设置长度
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            //构建一个HttpResponse
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+            //设置响应内容的格式  文字 图片
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            //设置长度
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 
-        //把响应内容刷到客户端
-        channelHandlerContext.writeAndFlush(response);
+            //把响应内容刷到客户端
+            channelHandlerContext.writeAndFlush(response);
+        }
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel注册");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel移除");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel激活状态");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel不活动状态");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel读取数据完毕");
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("channel用户事件触发");
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel可写更改");
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("channel发生异常");
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("助手类添加");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("助手类移除");
+        super.handlerRemoved(ctx);
     }
 }
---
+++ src/main/java/com/mistra/nw/websocketserver/WebSocketServer.java
@@ -0,0 +1,34 @@
+package com.mistra.nw.websocketserver;
+
+import com.mistra.nw.helloNettyServer.HelloServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @ Author: WangRui
+ * @ Version: 1.0
+ * @ Time: 2019/7/9 23:48
+ * @ Description:
+ */
+public class WebSocketServer {
+
+    private static void main(String[] args) throws InterruptedException {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(mainGroup, subGroup)// 设置主从线程组
+                    .channel(NioServerSocketChannel.class)//设置Nio的双向通道
+                    .childHandler(new HelloServerInitializer());//设置子处理器 用于处理workEventLoopGroup
+            ChannelFuture channelFuture = serverBootstrap.bind(8088).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+
+    }
+}
